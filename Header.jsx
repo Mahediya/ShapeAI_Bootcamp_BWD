421f1528eeb0f055aa8ad7276c2852b125188734
Mahediya <85402721+Mahediya@users.noreply.github.com>
2021-06-06 22:23:47 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <div>
+        <center>
+          <h1> BTS </h1>
+        </center>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div>
+      <div className="note">
+        <h1> RM </h1>
+        <p>
+          Real name is Kim Namjoon. He is the leader of BTS. He is 26 years old.
+          He is the main rapper of the Group.
+        </p>
+      </div>
+      <div className="note">
+        <h1> Jin </h1>
+        <p>
+          Real name is Kim Seok-jin. He is the eldest member in the group. He is
+          28 years old. He is one of Vocals of Group.
+        </p>
+      </div>
+      <div className="note">
+        <h1> SUGA </h1>
+        <p>
+          Real name is Min Yoon-gi. He is 28 year old. He is one of the rapper
+          of the Group.
+        </p>
+      </div>
+      <div className="note">
+        <h1> J-Hope </h1>
+        <p>
+          Real name is Jung Ho-seok. He is 27 year old. He is one of the rapper
+          of the Group.
+        </p>
+      </div>
+      <div className="note">
+        <h1> Jimin </h1>
+        <p>
+          Real name is Park Jimin. He is 25 year old. He is the lead vocalist of
+          the Group.
+        </p>
+      </div>
+      <div className="note">
+        <h1> V </h1>
+        <p>
+          Real name is Kim Tae-hyung. He is 23 year old. He is one of the
+          vocalist of the Group.
+        </p>
+      </div>
+      <div className="note">
+        <h1> JungKook </h1>
+        <p>
+          Real name is Jeon JungKook. He is the youngest member of BTS. He is 26
+          year old. He is the main Vocalist of the Group.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Note;
